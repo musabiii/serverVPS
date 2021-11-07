f18ddc54786e66d8735135a93305e9f3e0c364c1
musabii <musabi@live.com>
2021-11-07 17:32:35 +0300

    ready

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 const express = require('express');
+
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send('hi');
+});
+
+app.listen(8080,()=>{
+    console.log('8080 is started');
+});
